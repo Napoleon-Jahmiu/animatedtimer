7a8f2058e81af5d817cba07fbec7e5c26cedab3b
Napoleon-Jahmiu <Napoleonjay09mail.com>
2020-06-16 19:24:46 +0100

    finished animated timer

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ let count = 0;
 
  // This makes javaScript to determine the height of the the coundownArea
  const heightCountdown = countdownArea.getBoundingClientRect().height;
-
+// create the timer when page loads
 setTimer();
 
 resetBtn.addEventListener('click', setTimer)
 
+// create the timer that creates the timer
 function setTimer() {
     clearInterval(intervalTime);
     count = 0;
@@ -25,12 +26,16 @@ function setTimer() {
         /* this section treats the countdown height in pixels multiply by the 
         the number of counts that it will take for the user to wait for the next function to run 
         i.e from 10 to 0*/
+
+        // calculate the offset and apply it
         const offsetTime = heightCountdown * count;
         // This section is for the numbers to start moving vertically along the Y axis from upward
         numbersArea.style.transform = `translateY(-${offsetTime}px)`;
         
         /* This functions is saying stop the countdown at the last number and run the next function if there's any.
         it ensures that the countdown doesn't just disapper from the screen*/
+
+        // what happens when the countdown is done.
         if (count >= 10) {
             // at this point, the next function is suppose to run
             clearInterval(intervalTime);
